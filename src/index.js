319138b21a8bed87613b0c53beb230b759c6e78e
Billy Mohajeri <billy.mohajeri@gmail.com>
2023-01-30 19:20:49 +0200

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { Provider } from 'react-redux';
+import store from './redux/configureStore';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import store from './redux/configureStore';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
