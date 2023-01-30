eb1b384e4b9bae16f5847d60bee753bf7e3f804a
Billy Mohajeri <billy.mohajeri@gmail.com>
2023-01-30 18:57:04 +0200

    Fix linter errors

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,12 +1,9 @@
 import { useDispatch, useSelector } from 'react-redux';
 import React, { useEffect } from 'react';
-// import Rocket from './Rocket';
 import { getRockets } from '../redux/rockets/rockets';
 
 const Rockets = () => {
-//   const rocketsList = useSelector((state) => state.rockets);
   const dispatch = useDispatch();
-  //   console.log(rocketsList);
   const state = useSelector((state) => state.rockets);
 
   useEffect(() => {
@@ -15,12 +12,7 @@ const Rockets = () => {
     }
   }, [dispatch, state]);
 
-  return (
-    <></>
-    // <div className="rockets-container">
-    //   {rocketsList.map((r) => r.map((r) => <Rocket key={r.id} rocket={r} />))}
-    // </div>
-  );
+  return <></>;
 };
 
 export default Rockets;
