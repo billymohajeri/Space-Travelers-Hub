7afef16c21ebee84180cdf0484524c703cf1b3e2
Billy Mohajeri <billy.mohajeri@gmail.com>
2023-01-30 18:44:49 +0200

    Fetch rockets

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,3 +1,26 @@
-const Rockets = () => <h2>Hello fome Rockets page</h2>;
+import { useDispatch, useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+// import Rocket from './Rocket';
+import { getRockets } from '../redux/rockets/rockets';
+
+const Rockets = () => {
+  const rocketsList = useSelector((state) => state.rockets);
+  const dispatch = useDispatch();
+  console.log(rocketsList);
+  const state = useSelector((state) => state.rockets);
+
+  useEffect(() => {
+    if (state.length === 0) {
+      dispatch(getRockets());
+    }
+  }, [dispatch, state]);
+
+  return (
+    <></>
+    // <div className="rockets-container">
+    //   {rocketsList.map((r) => r.map((r) => <Rocket key={r.id} rocket={r} />))}
+    // </div>
+  );
+};
 
 export default Rockets;
