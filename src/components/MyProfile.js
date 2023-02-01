b8e57c95e4444f6b5523cd9f8d3018f1b4bc9295
Billy Mohajeri <billy.mohajeri@gmail.com>
2023-02-01 10:13:29 +0200

    Display reserved rockets in my profile page

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,3 +1,24 @@
-const myProfile = () => <h2>Hello fome My Profile page</h2>;
+import { useSelector } from 'react-redux';
+import '../styles/MyProfile.css';
 
-export default myProfile;
+const MyProfile = () => {
+  const rocketsList = useSelector((state) => state.rockets);
+  const myReserveRockets = rocketsList.filter((rockets) => rockets.reserved);
+
+  return (
+    <table>
+      <thead>
+        <th className="my-profile-thead">My Rockets</th>
+      </thead>
+      <tbody>
+        {myReserveRockets.map((rocket) => (
+          <tr key={rocket.id}>
+            <td>{rocket.rocket_name}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default MyProfile;
